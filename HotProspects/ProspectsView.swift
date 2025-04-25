23c78b6534b45b53e25e3e8e0740001540391c89
xHadi3 <hadiessam9@gmail.com>
2025-04-25 16:56:56 +0300

    adding the tabView

---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Hadi Al zayer on 27/10/1446 AH.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
